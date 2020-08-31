db2a7f0c91b36944bb92141b0a7246f8115cf550
SANSKAR GUPTA <54939968+sanskar1419@users.noreply.github.com>
2020-08-31 17:21:54 +0530

    Add files via upload

---
+++ Capstone Projects/Travel Arrangement (cabs + travel [air, road, rail, sea] + stay )/G1ANT.Addon.Goibibo/Commands/GoibiboCloseTabCommand.cs
@@ -0,0 +1,29 @@
+﻿using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.Goibibo
+{
+    [Command(Name = "Goibibo.closetab", Tooltip = "This command closes the current tab in the current browser")]
+    public class GoibiboCloseTabCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public GoibiboCloseTabCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.CurrentWrapper.CloseTab(arguments.Timeout.Value);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while closing current tab. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
