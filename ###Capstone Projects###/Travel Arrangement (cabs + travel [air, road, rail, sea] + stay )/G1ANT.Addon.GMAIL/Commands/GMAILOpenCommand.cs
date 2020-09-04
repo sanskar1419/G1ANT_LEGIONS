0cd6f4881ae52ae2cc7d6373c7a165912deca6eb
SANSKAR GUPTA <54939968+sanskar1419@users.noreply.github.com>
2020-09-04 21:31:56 +0530

    Add files via upload

---
+++ ###Capstone Projects###/Travel Arrangement (cabs + travel [air, road, rail, sea] + stay )/G1ANT.Addon.GMAIL/Commands/GMAILOpenCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace G1ANT.Addon.GMAIL
+{
+    [Command(Name = "gmail.open", Tooltip = "This command opens a new instance of GMAIL and optionally navigates to a specified URL address")]
+    public class GMAILOpenCommand : Command
+    {
+        public class Arguments : GMAILCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Name of a web browser")]
+            public TextStructure Type { get; set; }
+
+            [Argument(DefaultVariable = "Url", Tooltip = "URL address of a webpage to be loaded")]
+            public TextStructure Url { get; set; } = new TextStructure("https://www.google.com/intl/en-GB/gmail/about/#");
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+            [Argument(Tooltip = "By default, waits until the webpage fully loads")]
+            public BooleanStructure NoWait { get; set; } = new BooleanStructure(false);
+
+            [Argument(Tooltip = "Name of a variable where the command's result will be stored")]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
+        }
+        public GMAILOpenCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                    arguments.Type.Value,
+                    arguments.Url?.Value,
+                    arguments.Timeout.Value,
+                    arguments.NoWait.Value,
+                    Scripter.Log,
+                    Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new IntegerStructure(wrapper.Id));
+            }
+            catch (DriverServiceNotFoundException ex)
+            {
+                throw new ApplicationException("Driver not found", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Url '{arguments.Url.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
+
---
+++ ###Capstone Projects###/Travel Arrangement (cabs + travel [air, road, rail, sea] + stay )/G1ANT.Addon.GMAIL/Commands/GMAILSendMailsCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.GMAIL
+{
+    [Command(Name = "gmail.sendmails", Tooltip = "Allows user to send mails to GMAIL")]
+    class GMAILSendMailsCommand : Command
+    {
+        public GMAILSendMailsCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : GMAILCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Send Recipient Email ID")]
+            public TextStructure to { get; set; } = new TextStructure("");
+
+            [Argument(Required = true, Tooltip = "Enter your subject")]
+            public TextStructure subject { get; set; } = new TextStructure("");
+
+            [Argument(Required = true, Tooltip = "Enter message")]
+            public TextStructure message { get; set; } = new TextStructure("");
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                arguments.Search.Value = "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "to";
+                arguments.By.Value = "name";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.to.Value, arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "subjectbox";
+                arguments.By.Value = "name";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.subject.Value, arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = ":ou";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.message.Value, arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = ":nh";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+
+            }
+        }
+    }
+}
