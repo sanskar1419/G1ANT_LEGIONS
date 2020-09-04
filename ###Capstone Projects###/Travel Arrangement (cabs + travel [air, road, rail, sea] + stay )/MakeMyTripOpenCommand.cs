ed43ff3c1bdb0fddf5da82656272f377512fe6a6
sam337 <62427079+sam337@users.noreply.github.com>
2020-09-04 21:17:14 +0530

    Add files via upload

---
+++ ###Capstone Projects###/Travel Arrangement (cabs + travel [air, road, rail, sea] + stay )/MakeMyTripOpenCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.MakeMyTrip
+{
+    [Command(Name = "makemytrip.open", Tooltip = "This command opens make my trip on web browser ")]
+    public class MakeMyTripOpenCommand : Command
+    {
+        public MakeMyTripOpenCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        "chrome",
+                        "https://www.makemytrip.com/",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                var len = SeleniumManager.CurrentWrapper.RunScript("return document.getElementsByClassName(\"captcha -internal\").length");
+                if (len == "1")
+                {
+                    RobotMessageBox.Show("Captcha detected, please solve the captcha");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ ###Capstone Projects###/Travel Arrangement (cabs + travel [air, road, rail, sea] + stay )/MakeMyTripRunScriptCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+
+using G1ANT.Language;
+
+
+namespace G1ANT.Addon.MakeMyTrip
+{
+    [Command(Name = "makemytrip.runscript", Tooltip = "This command runs Javascript code inside the web browser")]
+    public class MakeMyTripRunScriptCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Script to be executed in the web browser")]
+            public TextStructure Script { get; set; }
+
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after the script execution")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+            [Argument(Tooltip = "Name of a variable where the command's result will be stored")]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
+        }
+
+        public MakeMyTripRunScriptCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                var result = SeleniumManager.CurrentWrapper.RunScript(arguments.Script.Value, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(result));
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while running javascript script: '{arguments.Script.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
