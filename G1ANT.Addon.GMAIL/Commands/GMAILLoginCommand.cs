32b0cbcb46742d10166e3a0e47db439b9acee656
SANSKAR GUPTA <54939968+sanskar1419@users.noreply.github.com>
2020-09-04 22:53:51 +0530

    Add files via upload

---
+++ G1ANT.Addon.GMAIL/Commands/GMAILLoginCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Addon.GMAIL;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.GMAIL
+{
+    [Command(Name = "gmail.login", Tooltip = "Allows the user login GMAIL for sending and recieving mails")]
+    class GMAILLoginCommand : Command
+    {
+        public GMAILLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : GMAILCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter your Email ID here")]
+            public TextStructure email { get; set; }
+
+            [Argument(Required = true, Tooltip = "Enter your password")]
+            public TextStructure pword { get; set; }
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                    "chrome",
+                    "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin",
+                    arguments.Timeout.Value,
+                    false,
+                    Scripter.Log,
+                    Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.email.Value, arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "password";
+                arguments.By.Value = "name";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pword.Value, arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                RobotMessageBox.Show("Enter two step process if applicable");
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while typing text to element. Text: '{arguments.email.Value}'. 'Search element phrase: '{arguments.By.Value}'.'{arguments.pword.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+
+    }
+}
