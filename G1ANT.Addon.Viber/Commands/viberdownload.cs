27bb7e4eb96375150366956ba7450b8b6757e054
SANSKAR GUPTA <54939968+sanskar1419@users.noreply.github.com>
2020-09-04 21:04:58 +0530

    Add files via upload

---
+++ G1ANT.Addon.Viber/Commands/viberdownload.cs
@@ -0,0 +1,48 @@
+﻿/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Selenium
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+
+using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.viber
+{
+    [Command(Name = "viber.download", Tooltip = "This command downloads viber")]
+    public class viberdownloadCommand : Command
+    {
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after clicking the specified element")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public viberdownloadCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div/div/main/div[1]/div/div[1]/div/a[1]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                arguments.Search.Value = "/html/body/div/div/main/div/div[2]/div/div[2]/span[1]/a";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while clicking element. Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ G1ANT.Addon.Viber/Commands/viberfeatures.cs
@@ -0,0 +1,45 @@
+﻿/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Selenium
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+
+using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.viber
+{
+    [Command(Name = "viber.features", Tooltip = "This command opens features page")]
+    public class viberfeaturesCommand : Command
+    {
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after clicking the specified element")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public viberfeaturesCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div/div/main/div[2]/div/div/div/div[3]/a";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while clicking element. Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
