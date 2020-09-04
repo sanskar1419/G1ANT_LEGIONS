9db3b5170f280dbacd36b1a2b54577bd1fa8d50d
SANSKAR GUPTA <54939968+sanskar1419@users.noreply.github.com>
2020-09-04 17:30:14 +0530

    Add files via upload

---
+++ G1ANT.Addon.Twitter/Commands/TwitterAlertActionCommand.cs
@@ -0,0 +1,32 @@
+﻿using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.Twit
+{
+    [Command(Name = "Twitter.alertaction", Tooltip = "Waits for an alert and performs a specified action")]
+    public class TwitterAlertActionCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Specifies an action: `accept` or `dismiss`")]
+            public TextStructure Action { get; set; }
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public TwitterAlertActionCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.CurrentWrapper.AlertPerformAction(arguments.Action.Value, arguments.Timeout.Value);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while performing alert action. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ G1ANT.Addon.Twitter/Commands/TwitterGetTextCommand.cs
@@ -0,0 +1,37 @@
+﻿using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.Twit
+{
+    [Command(Name = "Twitter.gettext", Tooltip = "This command reads text (a value) of a specified element")]
+
+    public class TwitterGetTextCommand : Command
+    {
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+            [Argument(Tooltip = "Name of a variable where the value of a specified attribute will be stored")]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
+        }
+        public TwitterGetTextCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                var attributeValue = SeleniumManager.CurrentWrapper.GetTextValue(
+                arguments,
+                arguments.Timeout.Value);
+
+                Scripter.Variables.SetVariableValue(arguments.Result.Value, new TextStructure(attributeValue));
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
